7fa30d4d4bde6c64cb6da13088e70fdf4f075234
hindhumuruganantham2007-priya <hindhumuruganantham2007@gmail.com>
2026-03-24 10:27:27 +0530

    Update program1.c

--- program1.c
+++ program1.c
@@ -1,74 +1,76 @@
-#include <stdio.h>
-
+include <stdio.h>
 int main() {
-    int r1, c1, r2, c2;
-    int A[10][10], B[10][10], C[10][10];
-    int i, j, k;
-
-    printf("Enter the number of rows of First Matrix: ");
-    scanf("%d", &r1);
-
-    printf("Enter the number of columns of First Matrix: ");
-    scanf("%d", &c1);
-
-    printf("Enter the elements of First Matrix:\n");
-    for(i = 0; i < r1; i++) {
-        for(j = 0; j < c1; j++) {
-            printf("A[%d][%d]: ", i, j);
-            scanf("%d", &A[i][j]);
-        }
-    }
-
-    printf("Enter the number of rows of Second Matrix: ");
-    scanf("%d", &r2);
-
-    printf("Enter the number of columns of Second Matrix: ");
-    scanf("%d", &c2);
-
-    printf("Enter the elements of Second Matrix:\n");
-    for(i = 0; i < r2; i++) {
-        for(j = 0; j < c2; j++) {
-            printf("B[%d][%d]: ", i, j);
-            scanf("%d", &B[i][j]);
-        }
-    }
-
-    // Matrix Multiplication
-    for(i = 0; i < r1; i++) {
-        for(j = 0; j < c2; j++) {
-            C[i][j] = 0;
-            for(k = 0; k < c1; k++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-
-    printf("\nFirst Matrix:\n");
-    for(i = 0; i < r1; i++) {
-        printf("[");
-        for(j = 0; j < c1; j++) {
-            printf("%d ", A[i][j]);
-        }
-        printf("]\n");
-    }
-
-    printf("\nSecond Matrix:\n");
-    for(i = 0; i < r2; i++) {
-        printf("[");
-        for(j = 0; j < c2; j++) {
-            printf("%d ", B[i][j]);
-        }
-        printf("]\n");
-    }
-
-    printf("\nResultant Matrix:\n");
-    for(i = 0; i < r1; i++) {
-        printf("[");
-        for(j = 0; j < c2; j++) {
-            printf("%d ", C[i][j]);
-        }
-        printf("]\n");
-    }
-
-    return 0;
+int A[10][10],
+B[10][10], 
+R[10]
+[10], r, c, i, j, k, ch;
+printf("Rows and
+columns: 
+");
+scanf("%d%d",&r,
+&c);
+printf("Matrix
+A:\n");
+for(i=0;i<r;i++)
+for(j=0;j<c;j++)
+scanf("%d",&A[i]
+[j]);
+printf("Matrix
+B:\n");
+for(i=0;i<r;i++)
+for(j=0;j<c;j++)
+scanf("%d",&B[i]
+[j]);
+printf("1.Add
+2.Sub 
+3.Mul
+4.Transpose
+A\nChoice: 
+");
+scanf("%d",&ch);
+if(ch==1){
+for(i=0;i<r;i++){
+for(j=0;j<c;j++){
+R[i][j]=A[i][j]+B[i]
+[j]; 
+printf("%d
+",R[i][j]); 
 }
+printf("\n"); } }
+else if(ch==2){
+for(i=0;i<r;i++){
+for(j=0;j<c;j++){
+R[i][j]=A[i][j]-B[i]
+[j]; 
+printf("%d
+",R[i][j]); 
+}
+printf("\n"); } }
+else if(ch==3){
+for(i=0;i<r;i++){
+for(j=0;j<c;j++){
+R[i][j]=0;
+for(k=0;k<c;k++)
+Rresult[i][j] += A[i][k] * B[k][j];
+ }
+ printf("Result of Multiplication:\n");
+ for(i=0;i<r1;i++){
+ for(j=0;j<c2;j++)
+ printf("%d ", result[i][j]);
+ printf("\n");
+ }
+ break;
+ case 4: // Transpose of A
+ printf("Transpose of Matrix A:\n");
+ for(i=0;i<c1;i++){
+ for(j=0;j<r1;j++)
+ printf("%d ", A[j][i]);
+ printf("\n");
+ }
+ break;
+ default:
+ printf("Invalid choice!\n");
+ }
+ return 0;
+}
+    
